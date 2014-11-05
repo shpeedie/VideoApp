8a8ad235786120479facb31d7da58bde80d3e154
shpeedie <timmo@australia.id.au>
2014-11-05 20:03:07 +1100

    Update application.rb
    added end!

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Testify
             g.stylesheets false
             g.javascripts false
             g.test false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
